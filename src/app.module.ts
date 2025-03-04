59762c9b29c24a2435e51db499161713b8b97fb0
Mursaleen00 <playessport@gmail.com>
2025-03-04 16:09:33 +0500

    `Feat`: - Remove app service - Add hello world on first page

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 
@@ -22,7 +21,7 @@ import { UserModule } from './user/user.module';
     UserModule,
     AuthModule,
   ],
-  providers: [AppService],
+  providers: [],
   controllers: [AppController],
 })
 export class AppModule {}
