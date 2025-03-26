4531784169a75d804b556d749d9621723d2032a8
Mursaleen00 <playessport@gmail.com>
2025-03-26 17:19:56 +0500

    `fix`

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,10 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller()
 export class AppController {
-  // constructor() {}
-  // @Get()
-  // getHello(): string {
-  //   return 'Hello World! Go to /api for APIs';
-  // }
+  constructor() {}
+  @Get()
+  getHello(): string {
+    return 'Hello World! Go to /api for APIs';
+  }
 }
