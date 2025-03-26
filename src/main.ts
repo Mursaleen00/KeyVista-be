a61b60996db8be8a0d001a371449667dc8767bd6
Mursaleen00 <playessport@gmail.com>
2025-03-27 01:57:53 +0500

    `fix`

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import { NestFactory } from '@nestjs/core';
 import { SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { swaggerConfig, themeOptions } from './config/swagger.config';
-import 'module-alias/register';
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule, {
